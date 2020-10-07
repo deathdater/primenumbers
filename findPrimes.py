37f72f49caa97e9e16b533067fd0b0cb63abb7f6
Deathdater <aditya.juet@gmail.com>
2020-10-07 22:49:18 +0530

    Now Checks is the range provided is an integer.

--- findPrimes.py
+++ findPrimes.py
@@ -1,5 +1,26 @@
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
+def checkPrimeBetter(num):
+    i=2
+    while (i**2) <= num :
+        if num % i !=0:
+            i+=1
+        else:
+            num=0
+            break
+
+    if num !=0:
+        return num
+    else:
+        return num
+
+
+
+
+
+
+
+
 def checkPrime(num):
     i = 2
     while i < num:
@@ -16,19 +37,17 @@ def checkPrime(num):
 
 
 def checkPrimeWithin(number):
-    if number>0 and number.is_integer():
-        prime_list = []
-        while number > 0:
-            if (checkPrime(number) != 0):
-                prime_list.append(number)
-                # print(prime_list)
-                number -= 1
-            else:
-                number -= 1
-        return prime_list
-    else:
-        print("Invalid Number, please provide an positive integer value!")
-
+    number=int(number)
+    prime_list = []
+    while number > 1:
+        # if (checkPrime(number) != 0):
+        if (checkPrimeBetter(number) != 0):
+            prime_list.append(number)
+            # print(prime_list)
+            number -= 1
+        else:
+            number -= 1
+    return prime_list
 
 
 
